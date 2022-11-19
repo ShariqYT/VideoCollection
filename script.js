fc5b2e88ded215de99407461d994d6334fccad67
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2022-11-19 19:47:18 +0530

    Update v1.2

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ icon.onclick = function(){
         icon.src = "assets/img/moon.png";
     }
 }
+
+let listVideo = document.querySelectorAll(".video-list .vid");
+let mainVideo = document.querySelector(".main-video video");
+let title = document.querySelector(".main-video .title");
+
+listVideo.forEach(video =>{
+    video.onclick = () =>{
+        listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+    };
+});
