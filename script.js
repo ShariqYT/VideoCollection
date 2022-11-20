c3e64b66f480aa2880558776b870f8497eab87b7
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2022-11-20 11:22:24 +0530

    Update to v1.3

--- script.js
+++ script.js
@@ -3,21 +3,8 @@ var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark-mode");
     if(document.body.classList.contains("dark-mode")){
-        icon.src = "assets/img/sun.png";
+        icon.src = "../assets/img/sun.png";
     }else{
-        icon.src = "assets/img/moon.png";
+        icon.src = "../assets/img/moon.png";
     }
 }
-
-let listVideo = document.querySelectorAll(".video-list .vid");
-let mainVideo = document.querySelector(".main-video video");
-let title = document.querySelector(".main-video .title");
-
-listVideo.forEach(video =>{
-    video.onclick = () =>{
-        listVideo.forEach(vid => vid.classList.remove('active'));
-        video.classList.add('active');
-            let src = video.children[0].getAttribute('src');
-            mainVideo.src = src;
-    };
-});
