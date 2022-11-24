4d836b46568e117746eddf141358243c3354de8f
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2022-11-24 16:28:56 +0530

    Update to v1.5

--- script.js
+++ script.js
@@ -9,19 +9,3 @@ icon.onclick = function(){
     }
 }
 
-let suggestions = [
-
-]
-
-const inputBox = document.querySelector("input");
-const suggBox = document.querySelector(".autocom-box");
-
-inputBox.onkeyup = (e) => {
-    let userData = e.target.value;
-    let emptyArray = [];
-    if(userData){
-        emptyArray = suggestions.filter((data)=>{
-            return data = ''
-        })
-    }
-}
